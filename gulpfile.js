13b67a4d2ca24e9f9f8fa023787c127cd01fda43
Ben Dunkle <ben@field2.com>
2017-06-07 12:28:51 -0400

    updated dir names

--- gulpfile.js
+++ gulpfile.js
@@ -12,31 +12,20 @@ var gulp = require('gulp'),
 
 var sassOptions = { outputStyle: 'expanded' };
 
-// gulp.task('sass', function() {
-//     return gulp.src('./scss/**/*.scss')
-//         .pipe(autoprefixer('last 2 version'))
-//         .pipe(sourcemaps.init())
 
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('./build/css'))
-
-//     .pipe(sourcemaps.write('./build/css'))
-//         .pipe(browserSync.stream());
-// });
-
-gulp.task('sass', function () {
-  return gulp
-    .src('./scss/**/*.scss')
-
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.stream());
+gulp.task('sass', function(ss) {
+    pump([
+            gulp.src("./scss/**/*.scss"),
+            autoprefixer(),
+            sourcemaps.init(),
+            sass().on('error', sass.logError),
+            sourcemaps.write('./'),
+            gulp.dest("./public_html/css"),
+            browserSync.stream()
+        ],
+        ss
+    );
 });
-
-
 
 
 
@@ -44,7 +33,7 @@ gulp.task('uglify', function(cb) {
     pump([
             gulp.src('js/*.js'),
             uglify(),
-            gulp.dest('./build/js'),
+            gulp.dest('./public_html/js'),
             browserSync.stream()
         ],
         cb
@@ -52,51 +41,57 @@ gulp.task('uglify', function(cb) {
 
 });
 
-gulp.task('html', function() {
-    nunjucksRender.nunjucks.configure(['./build']);
-    return gulp.src('./html/*')
-        .pipe(nunjucksRender())
-        .pipe(gulp.dest('./build'))
-        .pipe(browserSync.stream());
+
+
+gulp.task('html', function(nj) {
+    nunjucksRender.nunjucks.configure(['./public_html']);
+
+    pump([
+
+            gulp.src('./html/*'),
+            nunjucksRender(),
+            gulp.dest('./public_html'),
+            browserSync.stream()
+        ],
+        nj
+    );
 
 });
 
 
+
+
 gulp.task('imagemin', function() {
     return gulp
-        .src(['source/*.jpg', 'source/*.png'])
+        .src(['bitmaps/*.jpg', 'bitmaps/*.png'])
         .pipe(imagemin())
-        .pipe(gulp.dest('./build/img'))
+        .pipe(gulp.dest('./public_html/img'))
                 .pipe(browserSync.stream());
 
 });
 
 gulp.task('svg', function() {
-    return gulp.src('./source/*.svg')
+    return gulp.src('./vectors/*.svg')
         .pipe(svgSymbols())
-        .pipe(gulp.dest("./build/svg"));
+        .pipe(gulp.dest("./public_html/svg"));
 });
 
 
-
-// gulp.task('bs-reload', function() {
-//     browserSync.reload();
-// });
 
 
 gulp.task('watch', ['sass', 'uglify', 'html', 'imagemin', 'svg'], function() {
     browserSync.init({
         server: {
-            baseDir: './build'
+            baseDir: './public_html'
         }
     });
     gulp.watch('./scss/*.scss', ['sass']).on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
-    gulp.watch(['./html/*'], ['html']);
+    gulp.watch(['./html/**/*'], ['html']);
     gulp.watch('./js/*.js', ['uglify']);
-    gulp.watch('./source/*.svg', ['sprites']);
-    gulp.watch(['./source/*.jpg', './source/*.png'], ['imagemin']);
+    gulp.watch('./vectors/*.svg', ['sprites']);
+    gulp.watch(['./bitmaps/*.jpg', './bitmaps/*.png'], ['imagemin']);
 });
 
 gulp.task('default', ['watch']);
